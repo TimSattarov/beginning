e1beaa74ecb53bd99f47d10876237430767ba8bc
Timur <69400291+TimSattarov@users.noreply.github.com>
2020-10-20 19:11:08 +0300

    1
    1

---
+++ Lesson01/Lesson01/Library.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson01
+{
+    public class Library : IEnumerable
+    {
+        public List<Book> books = new List<Book>()
+        {
+            new Book() {Author = "Александр Дюма", Title = "Граф Монте-Кристо", Id = 0},
+            new Book() {Author = "Михаил Булгаков", Title = "Мастер и Маргарита", Id = 5},
+            new Book() {Author = "Фёдор Достоевский", Title = "Преступление и наказание", Id = 12},
+            new Book() {Author = "Александр Пушкин", Title = "Руслан и Людмила", Id = 116},
+            new Book() {Author = "Эрнест Хемингуэй", Title = "Старик и море", Id = 652},
+            new Book() {Author = "Даниель Дефо", Title = "Робинзон Крузо", Id = 135},
+            new Book() {Author = "Александр Пушкин", Title = "Дубровский", Id = 62},
+            new Book() {Author = "Артур Конан Дойльн", Title = "Приключения Шерлока Холмса", Id = 1023},
+            new Book() {Author = "Джордж Оруэлл", Title = "1984", Id = 33},
+            new Book() {Author = "Александр Пушкин", Title = "Капитанская дочка", Id = 142},
+        };
+
+        public Book this[int number]
+        {
+            get
+            {
+                var book = books.FirstOrDefault(b => b.Id == number);
+                return book;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return books.GetEnumerator();
+        }
+    }
+}
---
+++ Lesson01/Lesson01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var library = new Library();
+
+            //Вывод на консоль всех книг, находящихся в библиотеке
+            Console.WriteLine("Библиотека содержит следующие книги:");
+            foreach (Book item in library)
+            {
+                Console.WriteLine($"Автор - {item.Author}, Название - {item.Title}, Инв.номер - {item.Id}");
+            }
+
+            Console.WriteLine("***********************");
+
+
+
+            //Вывод на консоль книги с определенным инв. номером 
+            Console.Write("Введите номер книги: ");
+            if (Int32.TryParse(Console.ReadLine(), out int num) == false)
+            {
+                // проверка ввода, если не число, то вывести номер за пределы списка номеров книг
+                num = -1;
+            }
+
+            Book book = library[num];
+            if (book != null)
+            {
+                Console.WriteLine($"Автор - {book.Author}, Название - {book.Title}, Инв.номер - {book.Id}");
+            }
+            else Console.WriteLine("Книга не найдена");
+
+            Console.ReadLine();
+        }
+    }
+}
